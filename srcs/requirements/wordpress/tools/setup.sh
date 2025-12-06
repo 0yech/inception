17ace6b5f8f07c08e68bb247e8c43f52e5cf0f8b
0yech <Nelson.rey02@gmail.com>
2025-12-06 23:17:59 +0100

    added redis

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -18,8 +18,28 @@ sed -i "s/username_here/${MYSQL_USER}/" $WP_PATH/wp-config.php
 sed -i "s/password_here/${MYSQL_PASSWORD}/" $WP_PATH/wp-config.php
 sed -i "s/localhost/mariadb/" $WP_PATH/wp-config.php
 
+# Redis config
+if ! grep -q "WP_REDIS_HOST" "$WP_PATH/wp-config.php"; then
+    sed -i "/define( 'WP_DEBUG'/i \
+define('WP_REDIS_HOST', 'redis');\n\
+define('WP_REDIS_PORT', 6379);\n\
+define('WP_CACHE', true);\n\
+define('WP_CACHE_KEY_SALT', 'inception_42_');\n" \
+    $WP_PATH/wp-config.php
+fi
+
 # Fix file permissions
 chown -R www-data:www-data $WP_PATH
+
+# Install Redis plugin if not installed
+if ! wp plugin is-installed redis-cache --path=$WP_PATH --allow-root; then
+    wp plugin install redis-cache --activate --path=$WP_PATH --allow-root
+fi
+
+# Enable Redis cache
+if ! wp redis status --path=$WP_PATH --allow-root | grep -q "Connected"; then
+    wp redis enable --path=$WP_PATH --allow-root
+fi
 
 # Prepare PHP-FPM directory
 mkdir -p /run/php
